fdff7857f53d752507e35e11f342c8fef14a0b64
Václav Končický <koncicky@kam.mff.cuni.cz>
2022-09-13 23:33:46 +0200

    Prediction serialization and preparation for sending

--- src/commonMain/kotlin/Question.kt
+++ src/commonMain/kotlin/Question.kt
@@ -11,17 +11,16 @@ data class Question(
 @kotlinx.serialization.Serializable
 sealed interface AnswerSpace {
     val bins: Int
+    fun verifyPrediction(prediction: Prediction): Boolean
 }
-
-@kotlinx.serialization.Serializable
-data class NumericParams(
-    val mean: Double,
-    val stdDev: Double,
-)
 
 @kotlinx.serialization.Serializable
 class BinaryAnswerSpace() : AnswerSpace {
     override val bins: Int = 2
+    override fun verifyPrediction(prediction: Prediction): Boolean {
+        val pred = prediction as? BinaryPrediction ?: return false
+        return (pred.estimate in 0.0..1.0)
+    }
 }
 
 @kotlinx.serialization.Serializable
@@ -29,4 +28,30 @@ class NumericAnswerSpace(
     override val bins: Int,
     val min: Double,
     val max: Double,
-) : AnswerSpace
+) : AnswerSpace {
+    override fun verifyPrediction(prediction: Prediction): Boolean {
+        val pred = prediction as? NumericPrediction ?: return false
+        return (pred.mean in min..max && pred.stdDev in 0.0..(max-min)/2)
+    }
+}
+
+@kotlinx.serialization.Serializable
+sealed interface Prediction {
+}
+
+@kotlinx.serialization.Serializable
+data class NumericPrediction (
+    val mean: Double,
+    val stdDev: Double,
+) : Prediction
+
+@kotlinx.serialization.Serializable
+data class BinaryPrediction (
+    val estimate: Double
+) : Prediction
+
+@kotlinx.serialization.Serializable
+sealed interface Answer {
+    fun represent(): String
+    fun toBins(bins: Int): List<Double>
+}
--- src/jsMain/kotlin/components/QuestionList.kt
+++ src/jsMain/kotlin/components/QuestionList.kt
@@ -1,19 +1,22 @@
 package components
 
+import kotlinx.browser.document
+import kotlinx.browser.window
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import mui.material.*
 import react.*
 import space.kscience.dataforge.values.asValue
 import space.kscience.plotly.layout
 import space.kscience.plotly.models.*
 import tools.confido.distributions.*
-import tools.confido.question.BinaryAnswerSpace
-import tools.confido.question.NumericAnswerSpace
-import tools.confido.question.Question
+import tools.confido.question.*
 import utils.*
 
 
-external interface BinaryQuestionProps : Props {
-    var question : Question
+external interface QuestionAnswerFormProps<T : AnswerSpace> : Props {
+    var id: String
+    var answerSpace: T
 }
 
 fun mark(value: Number, label: String?) = jsObject {
@@ -21,10 +24,12 @@ fun mark(value: Number, label: String?) = jsObject {
     this.label = label
 }
 
+fun postPrediction(prediction: Prediction, answerSpace: AnswerSpace) {
+    window.alert(Json.encodeToString(prediction))
+}
 
-val NumericQuestion = FC<BinaryQuestionProps> {
-    val question = it.question
-    val answerSpace: NumericAnswerSpace = it.question.answerSpace as? NumericAnswerSpace ?: error("This is not numeric answer space!")
+val NumericQuestion = FC<QuestionAnswerFormProps<NumericAnswerSpace>> { props ->
+    val answerSpace = props.answerSpace
 
     var mean by useState(answerSpace.min)
     var stdDev by useState(0.0)
@@ -32,103 +37,112 @@ val NumericQuestion = FC<BinaryQuestionProps> {
     val dist = TruncatedNormalDistribution(mean, stdDev, answerSpace.min, answerSpace.max)
 
     fun sendPrediction() {
-        console.log("sending mean $mean and standard deviation $stdDev")
+        val pred = NumericPrediction(mean, stdDev)
+        postPrediction(pred, answerSpace)
     }
 
-    Accordion {
-        AccordionSummary {
-            id = question.id
-            Typography {
-                + question.name
-            }
+    Fragment {
+        DistributionPlot {
+            id = "${props.id}_plot"
+            min = answerSpace.min
+            max = answerSpace.max
+            step = 0.5
+            distribution = dist
+            confidences = listOf(
+                ConfidenceColor(0.9, "#333333".asValue()),
+                ConfidenceColor(0.7, "#666666".asValue()),
+                ConfidenceColor(0.5, "#999999".asValue())
+            )
         }
-        AccordionDetails {
 
-            DistributionPlot {
-                id = "${question.id}_plot"
-                min = answerSpace.min
-                max = answerSpace.max
-                step = 0.5
-                distribution = dist
-                confidences = listOf(ConfidenceColor(0.9, "#333333".asValue()), ConfidenceColor(0.7, "#666666".asValue()), ConfidenceColor(0.5, "#999999".asValue()))
-            }
-
-            Slider {
-                value = mean
-                min = answerSpace.min
-                max = answerSpace.max
-                step = 0.1
-                valueLabelDisplay = "auto"
-                onChange = { _, value, _ -> mean = value}
-                onChangeCommitted = { _,_ -> sendPrediction() }
-            }
-            Slider {
-                value = stdDev
-                min = 0
-                max = (answerSpace.max - answerSpace.min) / 2
-                step = 0.1
-                valueLabelDisplay = "auto"
-                onChange = { _, value, _ -> stdDev = value}
-                onChangeCommitted = { _,_ -> sendPrediction() }
-            }
-            listOf(0.9,0.7,0.5).map {p ->
-                Typography {
-                    val confidence = dist.confidenceInterval(1 - p)
-                    +"You are ${p * 100}% confident that the value lies between ${confidence.first.format(1)} and ${confidence.second.format(1)}"
-                }
+        Slider {
+            value = mean
+            min = answerSpace.min
+            max = answerSpace.max
+            step = 0.1
+            valueLabelDisplay = "auto"
+            onChange = { _, value, _ -> mean = value }
+            onChangeCommitted = { _, _ -> sendPrediction() }
+        }
+        Slider {
+            value = stdDev
+            min = 0
+            max = (answerSpace.max - answerSpace.min) / 2
+            step = 0.1
+            valueLabelDisplay = "auto"
+            onChange = { _, value, _ -> stdDev = value }
+            onChangeCommitted = { _, _ -> sendPrediction() }
+        }
+        listOf(0.9, 0.7, 0.5).map { p ->
+            Typography {
+                val confidence = dist.confidenceInterval(1 - p)
+                +"You are ${p * 100}% confident that the value lies between ${confidence.first.format(1)} and ${
+                    confidence.second.format(
+                        1
+                    )
+                }"
             }
         }
     }
 
 }
 
-val BinaryQuestion = FC<BinaryQuestionProps> {
-    val question = it.question
+val BinaryQuestion = FC<QuestionAnswerFormProps<BinaryAnswerSpace>> { props ->
+    var estimate by useState(50)
 
     fun formatPercent(value: Int): String = "$value %"
 
-    Accordion {
-        AccordionSummary {
-            id = question.id
-            Typography {
-                + question.name
-            }
-        }
-        AccordionDetails {
-            Slider {
-                defaultValue = 50
-                min = 0
-                max = 100
-                valueLabelDisplay = "auto"
-                valueLabelFormat = ::formatPercent
-                marks = arrayOf(
-                    mark(0, "0 %"),
-                    mark(100, "100 %"),
-                )
-            }
+    fun sendPrediction() {
+        val pred = BinaryPrediction(estimate / 100.0)
+        postPrediction(pred, props.answerSpace)
+    }
+
+    Fragment {
+        Slider {
+            defaultValue = 50
+            min = 0
+            max = 100
+            valueLabelDisplay = "auto"
+            valueLabelFormat = ::formatPercent
+            marks = arrayOf(
+                mark(0, "0 %"),
+                mark(100, "100 %"),
+            )
+            onChange = { _, value, _ -> estimate = value }
+            onChangeCommitted = { _, _ -> sendPrediction() }
         }
     }
 }
 
 external interface QuestionListProps : Props {
-    var questions : List<Question>
+    var questions: List<Question>
 }
 
-val QuestionList = FC<QuestionListProps> {props ->
+val QuestionList = FC<QuestionListProps> { props ->
     val visibleQuestions = props.questions.filter { it.visible }
 
-    visibleQuestions.map {question ->
-        when(question.answerSpace) {
-            is NumericAnswerSpace ->
-                NumericQuestion {
-                    this.question = question
-                    this.key = question.id
+    visibleQuestions.map { question ->
+        Accordion {
+            AccordionSummary {
+                id = question.id
+                Typography {
+                    +question.name
                 }
-            is BinaryAnswerSpace ->
-                BinaryQuestion {
-                    this.question = question
-                    this.key = question.id
+            }
+            AccordionDetails {
+                when (val answerSpace = question.answerSpace) {
+                    is NumericAnswerSpace ->
+                        NumericQuestion {
+                            this.id = question.id
+                            this.answerSpace = answerSpace
+                        }
+                    is BinaryAnswerSpace ->
+                        BinaryQuestion {
+                            this.id = question.id
+                            this.answerSpace = answerSpace
+                        }
                 }
+            }
         }
     }
 }
