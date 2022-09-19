4ee78e779b83cdf9f93a8d413adcd756ba6ab1ee
Václav Končický <koncicky@kam.mff.cuni.cz>
2022-09-19 17:50:45 +0200

    HTTP client singleton for frontend

--- src/jsMain/kotlin/Client.kt
+++ src/jsMain/kotlin/Client.kt
@@ -1,8 +1,31 @@
 import components.App
+import io.ktor.client.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import kotlinx.browser.document
+import kotlinx.coroutines.CoroutineScope
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlinx.coroutines.launch
 import react.create
 import react.dom.client.createRoot
+import tools.confido.payloads.SetName
 
+object Client {
+    val httpClient = HttpClient {
+        install(ContentNegotiation) {
+            json()
+        }
+    }
+
+    inline fun <reified T> postData(url: String, payload: T) = CoroutineScope(EmptyCoroutineContext).launch {
+        httpClient.post(url) {
+            contentType(ContentType.Application.Json.withParameter("charset", "utf-8"))
+            setBody(payload)
+        }
+    }
+}
 fun main() {
     val container = document.createElement("div")
     document.body!!.appendChild(container)
