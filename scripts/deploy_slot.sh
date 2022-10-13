8028f76d483dbc7803c0e7341648b92c63419194
Filip Stedronsky <p@regnarg.cz>
2022-10-13 11:01:09 +0200

    Quick-and-dirty deploy script. Use with care.

---
+++ scripts/deploy_slot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+slot="$1"
+[ -n "$slot" ] || exit 1
+./gradlew clean || exit 1
+./gradlew build || exit 1
+tmpd=$(mktemp -d)
+chmod a+rx "$tmpd" # permissions get copied to server, mktemp does 0700 by default
+tar xvf build/distributions/confido1-1.0-SNAPSHOT.tar --xform='s#^confido1-1.0-SNAPSHOT/##' --show-transformed-names -C "$tmpd" || exit 1
+mkdir "$tmpd/static"
+cp build/distributions/confido1.js "$tmpd/static" || exit 1
+gzip -k "$tmpd/static/confido1.js" || exit 1
+
+rsync -rlvc "$tmpd/" root@prod1.confido.tools:/usr/local/share/confido/"$slot"/
+
+
