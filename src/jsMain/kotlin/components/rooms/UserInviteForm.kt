d643bd7dd895f82c7485d84881f7f3d6c27f0074
Václav Končický <koncicky@kam.mff.cuni.cz>
2022-11-16 13:44:28 +0100

    Multiple user invitations: WIP
    Currently this crashes if you first add `a@ex`, then start typing `a@e`.

--- src/jsMain/kotlin/components/rooms/UserInviteForm.kt
+++ src/jsMain/kotlin/components/rooms/UserInviteForm.kt
@@ -19,42 +19,49 @@ import react.dom.html.HTMLAttributes
 import rooms.Forecaster
 import rooms.RoomPermission
 import rooms.RoomRole
-import tools.confido.refs.ref
+import tools.confido.refs.*
 import users.User
 
 internal sealed external interface UserAutocomplete
-internal data class ExistingUser(val user: User) : UserAutocomplete
+internal data class ExistingUser(val userRef: tools.confido.refs.Ref<User>) : UserAutocomplete
 internal data class NewUser(val email: String): UserAutocomplete
 
 internal fun renderInput(params: AutocompleteRenderInputParams) =
     TextField.create {
-        kotlinx.js.Object.assign(this, params)
+        Object.assign(this, params)
         placeholder = "User name or e-mail"
         label = ReactNode("Add a member")
     }
 
 internal fun groupBy(u: UserAutocomplete) = when(u) {
-    is ExistingUser -> if (u.user.type.isProper()) "Organization users" else "Guests"
+    is ExistingUser -> if (u.userRef.deref()?.type?.isProper() == true) "Organization users" else "Guests"
     is NewUser -> "Invite as guest"
 }
 
+internal fun isOptionEqualValue(option: UserAutocomplete, value: UserAutocomplete) =
+    if (value.asDynamic() is String) false else option == value
+
 internal fun getOptionLabel(option: UserAutocomplete) =
     when (option) {
-        is ExistingUser -> option.user.nick ?: option.user.email ?: ""
+        is ExistingUser -> {
+            val user = option.userRef.deref()
+            user?.nick ?: user?.email ?: ""
+        }
         is NewUser -> option.email
-        else -> option.toString()
     }
 internal fun renderOption(attributes: HTMLAttributes<HTMLLIElement>, option: UserAutocomplete, state: AutocompleteRenderOptionState) =
     ListItem.create {
         Object.assign(this, attributes)
         when (option) {
             is ExistingUser -> {
-                ListItemAvatar {
-                    UserAvatar {
-                        user = option.user
+                option.userRef.deref()?.let {
+                    ListItemAvatar {
+                        UserAvatar {
+                            user = it
+                        }
                     }
+                    +userListItemText(it)
                 }
-                +userListItemText(option.user)
             }
             is NewUser -> {
                 ListItemAvatar {
@@ -71,34 +78,50 @@ internal fun renderOption(attributes: HTMLAttributes<HTMLLIElement>, option: Use
 val UserInviteForm = FC<Props> {
     val (appState, stale) = useContext(AppStateContext)
     val room = useContext(RoomContext)
-    var chosenUser by useState<UserAutocomplete?>(null)
+    var chosenUsers by useState<Array<UserAutocomplete>>(emptyArray())
     var role by useState<RoomRole>(Forecaster)
 
     val members = room.members.filter {it.invitedVia == null}.map {it.user.id}.toSet()
     val emails = useMemo(appState.users) { appState.users.values.mapNotNull {it.email}.toSet() }
-    val users = useMemo(appState.users) {
-        appState.users.values.sortedWith(
+    val users = useMemo(appState.users, chosenUsers) {
+        val options = appState.users.values.sortedWith(
             compareBy({ it.type }, { (it.nick ?: it.email ?: "") })
         ).mapNotNull {
-            if (!it.isAnonymous() && it.id !in members) ExistingUser(it) else null
-        }.toTypedArray()
+            if (!it.isAnonymous() && it.id !in members) ExistingUser(it.ref) else null
+        }// + chosenUsers.filterIsInstance<NewUser>().sortedBy { it.email }
+
+        options.toTypedArray()
+    }
+    useEffect(chosenUsers) {
+        console.log(chosenUsers)
     }
 
-    val filterOptions: (ReadonlyArray<UserAutocomplete>, FilterOptionsState<UserAutocomplete>) -> ReadonlyArray<UserAutocomplete> = useMemo(emails) {{
+    val filterOptions: (ReadonlyArray<UserAutocomplete>, FilterOptionsState<UserAutocomplete>) -> ReadonlyArray<UserAutocomplete> = useMemo(users, chosenUsers) {{
         users, state ->
         val value = state.inputValue
+        var valueInOptions = false
 
         val filtered = users.filter {
-            val user = (it as ExistingUser).user
-            val inNick = user.nick?.contains(value, true) ?: false
-            val inEmail = user.email?.contains(value, true) ?: false
-            inNick || inEmail
+            when(it) {
+                is ExistingUser -> {
+                    val user = it.userRef.deref()
+                    val inNick = user?.nick?.contains(value, true) ?: false
+                    val inEmail = user?.email?.contains(value, true) ?: false
+                    valueInOptions = valueInOptions || user?.email?.equals(value, true) ?: false
+                    inNick || inEmail
+                }
+                is NewUser -> {
+                    val contains = it.email.contains(value, true)
+                    valueInOptions = valueInOptions || it.email.equals(value, true)
+                    contains
+                }
+            }
         }
 
         when {
-            (filtered.size == 1 && value == (filtered[0] as ExistingUser).user.email) ->
+            (filtered.size == 1 && value == ((filtered[0] as ExistingUser).userRef.deref()?.email ?: "")) ->
                 filtered.toTypedArray()
-            (value.contains("@") && !emails.contains(value)) ->
+            (value.contains("@") && !valueInOptions) ->
                 (filtered + listOf(NewUser(value))).toTypedArray()
             else -> filtered.toTypedArray()
         }
@@ -113,19 +136,21 @@ val UserInviteForm = FC<Props> {
 
         val autocomplete: FC<AutocompleteProps<UserAutocomplete>> = Autocomplete
         autocomplete {
+            multiple = true
             options = users
+            value = chosenUsers
             renderInput = ::renderInput
             renderOption = ::renderOption
             autoComplete = true
             getOptionLabel = ::getOptionLabel
+            //isOptionEqualToValue = ::isOptionEqualValue
             groupBy = ::groupBy
             ListboxComponent = List
             ListboxProps = utils.jsObject {
                 dense = true
             }.unsafeCast<ListProps>()
-            onChange = { _, value: UserAutocomplete, _, _ -> chosenUser = value }
+            onChange = { _, value: Array<UserAutocomplete>, _, _ -> chosenUsers = value }
             this.filterOptions = filterOptions
-            freeSolo = true
             fullWidth = true
         }
 
@@ -140,31 +165,31 @@ val UserInviteForm = FC<Props> {
             sx {
                 width = 7.rem
             }
-            disabled = chosenUser == null || stale
-            when(chosenUser) {
-                is NewUser -> {
+            disabled = chosenUsers.isEmpty() || stale
+            if (chosenUsers.any { it is NewUser }) {
                     endIcon = icons.SendIcon.create()
                     +"Invite"
                 }
-                else -> {
+                else {
                     startIcon = icons.AddIcon.create()
                     +"Add"
                 }
-            }
 
             onClick = {
-                when(val who = chosenUser) {
-                    is ExistingUser ->
-                        AddedExistingMember(who.user.ref, role)
-                    is NewUser -> {
-                        AddedNewMember(who.email, role)
+                chosenUsers.forEach {
+                    when (val who = it) {
+                        is ExistingUser ->
+                            AddedExistingMember(who.userRef, role)
+                        is NewUser -> {
+                            AddedNewMember(who.email, role)
+                        }
+                        null -> null
+                    }?.let { addedMember ->
+                        Client.postData("/rooms/${room.id}/members/add", addedMember)
                     }
-                    null -> null
-                }?.let {addedMember ->
-                    Client.postData("/rooms/${room.id}/members/add", addedMember)
-                    chosenUser = null
-                    role = Forecaster
                 }
+                chosenUsers = emptyArray()
+                role = Forecaster
             }
         }
     }
