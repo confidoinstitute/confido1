89657c4374abd20fdad2779443a5a68d17bee5d4
Jiří Sejkora <jirka@confido.tools>
2022-11-27 04:02:30 +0100

    Reduce width of user settings

--- src/jsMain/kotlin/components/profile/UserSettings.kt
+++ src/jsMain/kotlin/components/profile/UserSettings.kt
@@ -10,6 +10,7 @@ import payloads.requests.SetNick
 import payloads.requests.StartEmailVerification
 import react.dom.html.ReactHTML.br
 import users.UserType
+import utils.byTheme
 import utils.eventValue
 import utils.isEmailValid
 import utils.themed
@@ -54,150 +55,154 @@ val UserSettings = FC<Props> {
         nameUpdated = true
     }
 
-    Typography {
-        sx {
-            marginBottom = themed(4)
-        }
-        variant = TypographyVariant.h2
-        +"User settings"
-    }
+    Container {
+        maxWidth = byTheme("sm")
 
-    Stack {
-        spacing = themed(2);
-
-        if (user.type == UserType.GUEST && user.email == null && pendingEmailChange == null) {
-            Alert {
-                AlertTitle {
-                    +"No email is set!"
-                }
-                severity = AlertColor.error
-                +"It is impossible to log back in without having an email set. Please set an email below if you want to return later."
+        Typography {
+            sx {
+                marginBottom = themed(4)
             }
-        } else {
-            if (!user.emailVerified || pendingEmailChange != null) {
+            variant = TypographyVariant.h2
+            +"User settings"
+        }
+
+        Stack {
+            spacing = themed(2);
+
+            if (user.type == UserType.GUEST && user.email == null && pendingEmailChange == null) {
                 Alert {
                     AlertTitle {
-                        +"Your email is not verified!"
+                        +"No email is set!"
                     }
-                    severity = AlertColor.warning
-                    if (pendingEmailChange != null) {
-                        +"We have sent you a verification email, please check your inbox."
-                    } else {
-                        +"To resolve this, we need to send you a verification email."
-                    }
-                    br {}
-                    Box {
-                        sx {
-                            marginTop = themed(1)
+                    severity = AlertColor.error
+                    +"It is impossible to log back in without having an email set. Please set an email below if you want to return later."
+                }
+            } else {
+                if (!user.emailVerified || pendingEmailChange != null) {
+                    Alert {
+                        AlertTitle {
+                            +"Your email is not verified!"
                         }
-                        Button {
-                            onClick = {
-                                Client.postData("/profile/email/start_verification", StartEmailVerification(email))
+                        severity = AlertColor.warning
+                        if (pendingEmailChange != null) {
+                            +"We have sent you a verification email, please check your inbox."
+                        } else {
+                            +"To resolve this, we need to send you a verification email."
+                        }
+                        br {}
+                        Box {
+                            sx {
+                                marginTop = themed(1)
                             }
+                            Button {
+                                onClick = {
+                                    Client.postData("/profile/email/start_verification", StartEmailVerification(email))
+                                }
+                                disabled = stale
+
+                                if (pendingEmailChange != null) {
+                                    +"Resend verification email"
+                                } else {
+                                    +"Send verification email"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (nameUpdated) {
+                Alert {
+                    severity = AlertColor.success
+                    +"Your name has been updated."
+                }
+            }
+
+            val textFieldVariant = FormControlVariant.outlined
+
+            Card {
+                CardHeader {
+                    title = ReactNode("Profile")
+                }
+                CardContent {
+                    Stack {
+                        TextField {
+                            variant = textFieldVariant
+                            id = "name-field"
+                            label = ReactNode("Name")
+                            helperText =
+                                ReactNode("Your name may appear whenever you comment or make predictions. You can change it at any time.")
+                            value = name
                             disabled = stale
-
-                            if (pendingEmailChange != null) {
-                                +"Resend verification email"
-                            } else {
-                                +"Send verification email"
+                            onChange = {
+                                name = it.eventValue()
+                            }
+                            onKeyUp = {
+                                if (it.key == "Enter") {
+                                    changeName()
+                                }
                             }
                         }
                     }
                 }
-            }
-        }
-
-        if (nameUpdated) {
-            Alert {
-                severity = AlertColor.success
-                +"Your name has been updated."
-            }
-        }
-
-        val textFieldVariant = FormControlVariant.outlined
-
-        Card {
-            CardHeader {
-                title = ReactNode("Profile")
-            }
-            CardContent {
-                Stack {
-                    TextField {
-                        variant = textFieldVariant
-                        id = "name-field"
-                        label = ReactNode("Name")
-                        helperText = ReactNode("Your name may appear whenever you comment or make predictions. You can change it at any time.")
-                        value = name
+                CardActions {
+                    sx {
+                        padding = themed(2)
+                    }
+                    Button {
+                        onClick = { changeName() }
+                        variant = ButtonVariant.contained
                         disabled = stale
-                        onChange = {
-                            name = it.eventValue()
+
+                        if (user.nick == null) {
+                            +"Set name"
+                        } else {
+                            +"Update name"
                         }
-                        onKeyUp = {
-                            if (it.key == "Enter") {
-                                changeName()
+                    }
+                }
+            }
+
+            Card {
+                CardHeader {
+                    title = ReactNode("Email address")
+                }
+
+                CardContent {
+                    // TODO: Better explanation of email visibility
+                    Stack {
+                        TextField {
+                            variant = textFieldVariant
+                            id = "email-field"
+                            label = ReactNode("Email address")
+                            helperText = emailError?.let { ReactNode(it) }
+                                ?: ReactNode("This email address is used for logging into your account and may be shown to other members of the organization.")
+                            error = emailError != null
+                            required = true
+                            value = email
+                            disabled = stale
+                            onChange = {
+                                email = it.eventValue()
+                            }
+                            onKeyUp = {
+                                if (it.key == "Enter") {
+                                    changeEmail()
+                                }
                             }
                         }
                     }
                 }
-            }
-            CardActions {
-                sx {
-                    padding = themed(2)
-                }
-                Button {
-                    onClick = { changeName() }
-                    variant = ButtonVariant.contained
-                    disabled = stale
 
-                    if (user.nick == null) {
-                        +"Set name"
-                    } else {
-                        +"Update name"
+                CardActions {
+                    sx {
+                        padding = themed(2)
                     }
-                }
-            }
-        }
-
-        Card {
-            CardHeader {
-                title = ReactNode("Email address")
-            }
-
-            CardContent {
-                // TODO: Better explanation of email visibility
-                Stack {
-                    // TODO: full-width or not?
-                    TextField {
-                        variant = textFieldVariant
-                        id = "email-field"
-                        label = ReactNode("Email address")
-                        helperText = emailError?.let { ReactNode(it) }
-                            ?: ReactNode("This email address is used for logging into your account and may be shown to other members of the organization.")
-                        error = emailError != null
-                        required = true
-                        value = email
+                    Button {
+                        onClick = { changeEmail() }
+                        variant = ButtonVariant.contained
                         disabled = stale
-                        onChange = {
-                            email = it.eventValue()
-                        }
-                        onKeyUp = {
-                            if (it.key == "Enter") {
-                                changeEmail()
-                            }
-                        }
+                        +"Update email"
                     }
-                }
-            }
-
-            CardActions {
-                sx {
-                    padding = themed(2)
-                }
-                Button {
-                    onClick = { changeEmail() }
-                    variant = ButtonVariant.contained
-                    disabled = stale
-                    +"Update email"
                 }
             }
         }
