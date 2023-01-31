a6b06324234de2150785831f1fe7f05c18f576f1
Filip Stedronsky <p@regnarg.cz>
2023-01-31 17:29:16 +0100

    Deploy script: record git commit & local diff in output directory

--- scripts/deploy_slot.sh
+++ scripts/deploy_slot.sh
@@ -13,7 +13,10 @@ cp -r ./src/jvmMain/resources/* "$tmpd/static/" || exit 1
 
 gzip -k "$tmpd/static/confido1.js" || exit 1
 
+git rev-parse HEAD > "$tmpd/COMMIT_ID"
+if git diff HEAD | grep -q .; then git diff HEAD >"$tmpd/LOCAL_CHANGES.diff"; fi
+
 ssh=root@${server:-prod1.confido.tools}
-rsync -rlvc "$tmpd/" $ssh:/usr/local/lib/confido/"$slot"/
+rsync -rlvc --del "$tmpd/" $ssh:/usr/local/lib/confido/"$slot"/
 
 ssh $ssh 'for fn in $(grep -Fxl CONFIDO_SLOT='"$slot"' /etc/confido/*.env) ; do name="confido@$(basename "$fn" .env)"; echo "Restarting $name"; systemctl restart $name; done'
