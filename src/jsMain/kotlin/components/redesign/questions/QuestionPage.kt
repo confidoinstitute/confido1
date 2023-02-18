ab54ce682d3989859e28a0b7c4497d3ad6a90e71
Jiří Sejkora <jirka@confido.tools>
2023-02-18 21:27:14 +0100

    Redesign: core of question page

---
+++ src/jsMain/kotlin/components/redesign/questions/QuestionPage.kt
@@ -0,0 +1,269 @@
+package components.redesign.questions
+
+import components.AppStateContext
+import components.redesign.Comment
+import components.redesign.TextWithLinks
+import components.redesign.basic.Stack
+import csstype.*
+import emotion.react.css
+import hooks.useWebSocket
+import payloads.responses.CommentInfo
+import payloads.responses.WSData
+import react.FC
+import react.Props
+import react.dom.html.ReactHTML.button
+import react.dom.html.ReactHTML.div
+import react.useContext
+import react.useState
+import tools.confido.question.Question
+import tools.confido.refs.ref
+
+external interface QuestionLayoutProps : Props {
+    var question: Question
+}
+
+external interface QuestionHeaderProps : Props {
+    var text: String
+    var description: String
+}
+
+external interface QuestionStatusProps : Props {
+    var text: String
+}
+
+external interface QuestionEstimateSectionProps : Props {
+
+}
+
+external interface QuestionEstimateTabButtonProps : Props {
+    var text: String
+    var active: Boolean
+    var onClick: (() -> Unit)?
+}
+
+external interface QuestionCommentSectionProps : Props {
+    var question: Question
+}
+
+private val bgColor = Color("#f2f2f2")
+
+val QuestionPage = FC<QuestionLayoutProps> { props ->
+    val (appState, stale) = useContext(AppStateContext)
+    val prediction = appState.myPredictions[props.question.ref]
+
+    Stack {
+        QuestionHeader {
+            this.text = props.question.name
+            this.description = props.question.description
+        }
+        QuestionEstimateSection {
+            // TODO: connect to the question
+        }
+        QuestionCommentSection {
+            this.question = props.question
+        }
+    }
+}
+
+val QuestionEstimateTabButton = FC<QuestionEstimateTabButtonProps> { props ->
+    button {
+        css {
+            all = Globals.unset
+            cursor = Cursor.pointer
+
+            borderRadius = 10.px
+            padding = Padding(12.px, 10.px)
+
+            flexGrow = number(1.0)
+            textAlign = TextAlign.center
+
+            fontFamily = FontFamily.sansSerif
+            fontSize = 17.px
+            lineHeight = 21.px
+
+            if (props.active) {
+                backgroundColor = Color("#FFFFFF")
+                color = Color("#000000")
+                fontWeight = integer(500)
+            } else {
+                color = Color("rgba(0, 0, 0, 0.5)")
+                fontWeight = FontWeight.normal
+            }
+        }
+        onClick = { props.onClick?.invoke() }
+
+        +props.text
+    }
+}
+
+val QuestionEstimateSection = FC<QuestionEstimateSectionProps> { props ->
+    // false = your estimate open
+    // true = group estimate open
+    var groupEstimateOpen by useState(false)
+
+    // Tabs
+    Stack {
+        css {
+            padding = 15.px
+            background = bgColor
+            borderRadius = 5.px
+        }
+        direction = FlexDirection.row
+        QuestionEstimateTabButton {
+            text = "Your estimate"
+            active = !groupEstimateOpen
+            onClick = { groupEstimateOpen = false }
+        }
+        QuestionEstimateTabButton {
+            text = "Group estimate"
+            active = groupEstimateOpen
+            onClick = { groupEstimateOpen = true }
+        }
+    }
+
+    div {
+        css {
+            height = 196.px
+            backgroundColor = Color("#fff")
+        }
+        if (!groupEstimateOpen) {
+            // TODO: Your estimate
+            +"TODO your estimate goes here"
+        } else {
+            // TODO: Group estimate
+            +"TODO group estimate goes here"
+        }
+    }
+    Stack {
+        // TODO: Section describing the estimate
+        css {
+            padding = Padding(25.px, 15.px)
+            gap = 10.px
+        }
+        div {
+            +"This helpful section describes your estimate or the group estimate and how many people participated."
+        }
+        div {
+            +"This still needs to be implemented."
+        }
+    }
+}
+
+val QuestionHeader = FC<QuestionHeaderProps> { props ->
+    Stack {
+        css {
+            padding = Padding(20.px, 15.px, 5.px)
+            gap = 4.px
+            background = bgColor
+        }
+
+        // Question text
+        div {
+            css {
+                fontFamily = FontFamily.serif
+                fontWeight = FontWeight.bold
+                fontSize = 34.px
+                lineHeight = 105.pct
+                color = Color("#000000")
+            }
+            +props.text
+        }
+
+        // Question status
+        // TODO: Implement on backend
+        QuestionStatusLine {
+            text = "Opened 10 feb 2023, 12:00"
+        }
+        //QuestionStatusLine {
+        //    text = "Closing 26 feb 2023, 12:00"
+        //}
+        //QuestionStatusLine {
+        //    text = "Resolving 28 feb 2023, 12:00"
+        //}
+
+        // Question description
+        div {
+            css {
+                padding = Padding(10.px, 0.px, 0.px)
+                fontFamily = FontFamily.sansSerif
+                fontSize = 15.px
+                lineHeight = 18.px
+                color = Color("#000000")
+            }
+            // TODO: clamp and show "See more" if text too long
+            TextWithLinks {
+                text = props.description
+            }
+        }
+    }
+}
+
+val QuestionCommentSection = FC<QuestionCommentSectionProps> { props ->
+    val comments = useWebSocket<Map<String, CommentInfo>>("/state${props.question.urlPrefix}/comments")
+    Stack {
+        css {
+            justifyContent = JustifyContent.spaceBetween
+            padding = Padding(12.px, 13.px, 13.px, 15.px)
+            backgroundColor = bgColor
+        }
+        direction = FlexDirection.row
+        div {
+            css {
+                textTransform = TextTransform.uppercase
+                fontFamily = FontFamily.sansSerif
+                fontSize = 13.px
+                lineHeight = 16.px
+                color = Color("#777777")
+            }
+            +"Comments"
+        }
+        // TODO: Proper sorting component
+        div {
+            css {
+                fontFamily = FontFamily.sansSerif
+                fontWeight = integer(600)
+                fontSize = 13.px
+                lineHeight = 16.px
+                color = Color("#888888")
+            }
+            // TODO: Icon
+            +"Newest first"
+        }
+    }
+
+    Stack {
+        css {
+            gap = 8.px
+        }
+
+        when (comments) {
+            is WSData -> comments.data.entries.sortedByDescending { it.value.comment.timestamp }.map {
+                Comment {
+                    commentInfo = it.value
+                }
+            }
+
+            else -> {
+                // TODO: Proper loading design
+                +"Loading..."
+            }
+        }
+    }
+}
+
+val QuestionStatusLine = FC<QuestionStatusProps> { props ->
+    div {
+        css {
+            fontFamily = FontFamily.sansSerif
+            fontWeight = FontWeight.bold
+            fontSize = 12.px
+            lineHeight = 15.px
+            color = Color("rgba(0, 0, 0, 0.3)")
+
+            padding = Padding(2.px, 0.px)
+            textTransform = TextTransform.uppercase
+        }
+        +props.text
+    }
+}
+
