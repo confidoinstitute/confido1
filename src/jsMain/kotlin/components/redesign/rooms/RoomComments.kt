b47b4562a32402c9ea7a4541f3ee621cf608796f
Jiří Sejkora <jirka@confido.tools>
2023-02-19 22:41:32 +0100

    Redesign: adding comments for room discussions

--- src/jsMain/kotlin/components/redesign/rooms/RoomComments.kt
+++ src/jsMain/kotlin/components/redesign/rooms/RoomComments.kt
@@ -1,6 +1,9 @@
 package components.redesign.rooms
 
+import components.redesign.comments.AddCommentDialog
 import components.redesign.comments.Comment
+import components.redesign.comments.CommentInputVariant
+import components.redesign.forms.TextButton
 import components.rooms.RoomContext
 import csstype.*
 import emotion.react.css
@@ -10,11 +13,20 @@ import react.FC
 import react.Props
 import react.dom.html.ReactHTML.div
 import react.useContext
+import react.useState
 
 val RoomComments = FC<Props> {
     val room = useContext(RoomContext)
-
     val roomComments = useWebSocket<Map<String, CommentInfo>>("/state${room.urlPrefix}/comments")
+
+    var addCommentOpen by useState(false)
+
+    AddCommentDialog {
+        open = addCommentOpen
+        onClose = { addCommentOpen = false }
+        id = room.id
+        variant = CommentInputVariant.ROOM
+    }
 
     // TODO: Comment creation input
 
@@ -39,11 +51,17 @@ val RoomComments = FC<Props> {
             gap = 8.px
             marginTop = 8.px
         }
-        roomComments.data?.entries?.sortedBy { it.value.comment.timestamp }?.map {
+        roomComments.data?.entries?.sortedByDescending { it.value.comment.timestamp }?.map {
             Comment {
                 this.commentInfo = it.value
                 this.key = it.key
             }
+        }
+
+        // TODO: Replace with a proper UI
+        TextButton {
+            onClick = { addCommentOpen = true }
+            +"Add comment"
         }
     }
 }
