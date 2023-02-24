3fc801a2d4627ca57fa18f83b3011e3bf237a1ec
Filip Stedronsky <filip@confido.tools>
2023-02-24 16:47:44 +0100

    useWebSocket: fix relative paths

--- src/jsMain/kotlin/hooks/useWebSocket.kt
+++ src/jsMain/kotlin/hooks/useWebSocket.kt
@@ -26,22 +26,23 @@ enum class WebSocketState {
  */
 inline fun <reified T> useWebSocket(address: String, retryDelay: Int = 5000, clearOnStale: Boolean = false): WSResponse<T> {
     var value by useState<WSResponse<T>>(WSLoading())
+    val fullAddr = if ("://" in address) address else webSocketUrl(address)
 
-    useCoroutine(address) {
+    useCoroutine(fullAddr) {
         value = WSLoading()
         while (true) {
             try {
-                console.log("WS OPEN", address)
-                Client.httpClient.webSocket(address) {
+                console.log("WS OPEN", fullAddr)
+                Client.httpClient.webSocket(fullAddr) {
                     while (true) {
                         value = receiveDeserialized()
                     }
                 }
             } catch (e: CancellationException) {
-                console.log("WS CANCEL", address)
+                console.log("WS CANCEL", fullAddr)
                 throw e
             } catch (e: Exception) {
-                console.log("WS ERROR", address, "\n", e.toString())
+                console.log("WS ERROR", fullAddr, "\n", e.toString())
             }
             if (value is WSData && !clearOnStale) value = WSData(value.data!!, true)
             else value = WSLoading()
