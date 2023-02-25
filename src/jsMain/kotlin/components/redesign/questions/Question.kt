5722cbba2d9ae38268359c6742e2a8dcc1503b58
Filip Stedronsky <filip@confido.tools>
2023-02-25 17:26:23 +0100

    Redesign: autoredirect back to room from nonexistent question
    (useful
    e.g. after question disappears)

--- src/jsMain/kotlin/components/redesign/questions/Question.kt
+++ src/jsMain/kotlin/components/redesign/questions/Question.kt
@@ -1,19 +1,28 @@
 package components.redesign.questions
 
 import components.AppStateContext
+import components.rooms.RoomContext
 import hooks.useDocumentTitle
 import kotlinx.js.get
 import react.FC
 import react.Props
+import react.router.useNavigate
 import react.router.useParams
 import react.useContext
+import react.useEffect
 
 val Question = FC<Props> {
     val (appState, stale) = useContext(AppStateContext)
+    val roomCtx = useContext(RoomContext)
 
     val questionID = useParams()["questionID"] ?: return@FC
-    val question = appState.questions[questionID] ?: return@FC
+    val question = appState.questions[questionID]
+    val navigate = useNavigate()
+    useEffect(question) {
+        if (question == null) navigate(roomCtx.urlPrefix)
+    }
 
+    if (question != null)
     QuestionPage {
         this.question = question
     }
