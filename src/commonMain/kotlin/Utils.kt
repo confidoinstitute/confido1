aa14f2c09102907a5a9a18a7bf0abaf6bfb5abfd
Filip Stedronsky <filip@confido.tools>
2023-04-10 20:39:52 +0200

    common utils += List2

--- src/commonMain/kotlin/Utils.kt
+++ src/commonMain/kotlin/Utils.kt
@@ -3,9 +3,16 @@
 package tools.confido.utils
 
 import kotlinx.datetime.*
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotlin.js.JsName
 import kotlin.jvm.JvmName
 import kotlin.math.roundToInt
+import kotlin.math.sqrt
 
 operator fun Number.compareTo(b : Number): Int {
     if ((this is Int || this is Short || this is Long || this is Byte || this is Float || this is Double)
@@ -43,6 +50,43 @@ class GeneratedList<T>(override val size: Int, val gen: (Int) -> T) : List<T> {
     override fun iterator() = listIterator()
     override fun lastIndexOf(element: T) = (0 until size).reversed().first { gen(it) == element }
 
+}class List2Serializer<T>(private val elementSerializer: KSerializer<T>) : KSerializer<List2<T>> {
+    override val descriptor: SerialDescriptor = ListSerializer(elementSerializer).descriptor
+    override fun serialize(encoder: Encoder, value: List2<T>) {
+        encoder.encodeSerializableValue(ListSerializer(elementSerializer), value)
+    }
+    override fun deserialize(decoder: Decoder): List2<T> {
+        val lst = decoder.decodeSerializableValue(ListSerializer(elementSerializer))
+        return List2(lst)
+    }
+}
+
+// A fixed-size two element list. Unlinke a Pair, both elements are of the same type,
+// and it can be iterated over and use functions like map and zip. Used to pass two
+// objects of the same type, for example two probability distributions to compare.
+@Serializable(with=List2Serializer::class)
+open class List2<out T>(val lst: List<T>) : List<T> by lst {
+    init { require(lst.size == 2) }
+    constructor(e1: T, e2: T) : this(listOf(e1,e2))
+
+    inline fun <U> map(f: (T) -> U): List2<U> = List2(lst.map(f))
+    inline fun <U> zip(other: List2<U>): List2<Pair<T,U>> = List2(lst.zip(other))
+    inline fun <K: Comparable<K>> sortedBy(crossinline f: (T) -> K): List2<T> = List2(lst.sortedBy(f))
+    fun reversed() = List2(lst.reversed())
+    override val size get() = 2
+
+    // sometimes it is useful to index with booleans
+    operator fun get(idx: Boolean) = get(if(idx) 1 else 0)
+
+    val e1 get() = lst[0]
+    val e2 get() = lst[1]
+
+    fun component1() = lst[0]
+    fun component2() = lst[1]
+    // override extension functions List<T>.componentN() to get compile-time warning
+    fun component3(): Nothing = throw IndexOutOfBoundsException()
+    fun component4(): Nothing = throw IndexOutOfBoundsException()
+    fun component5(): Nothing = throw IndexOutOfBoundsException()
 }
 
 fun String.mapFirst(f: (Char) -> String): String = if (isEmpty()) "" else f(this[0]) + this.substring(1)
@@ -80,25 +124,47 @@ fun zeros(size: Int) = List(size) { _ -> 0.0 }
 @JsName("Zplus")
 @JvmName("Zplus")
 infix fun List<Double>.`Z+`(other: List<Double>) = zip(other) { x,y -> x+y }
+@JsName("Zplus2")
+@JvmName("Zplus2")
+infix fun List2<Double>.`Z+`(other: List2<Double>) = List2(zip(other) { x,y -> x+y })
 @JsName("Zminus")
 @JvmName("Zminus")
 infix fun List<Double>.`Z-`(other: List<Double>) = zip(other) { x,y -> x-y }
+@JsName("Zminus2")
+@JvmName("Zminus2")
+infix fun List2<Double>.`Z-`(other: List2<Double>) = List2(zip(other) { x,y -> x-y })
 @JsName("Ztimes1")
 @JvmName("Ztimes1")
 infix fun Number.`Z*`(other: List<Double>) = other.map { this.toDouble() * it }
 @JsName("Ztimes2")
 @JvmName("Ztimes2")
 infix fun List<Double>.`Z*`(other: Number) = this.map { other.toDouble() * it }
+
+@JsName("Ztimes3")
+@JvmName("Ztimes3")
+infix fun Number.`Z*`(other: List2<Double>) = List2(other.map { this.toDouble() * it })
+@JsName("Ztimes4")
+@JvmName("Ztimes4")
+infix fun List2<Double>.`Z*`(other: Number) = List2(this.map { other.toDouble() * it })
 // Aparently, `Z/` is not a valid identifier.
 infix fun Number.Zdiv(other: List<Double>) = other.map { this.toDouble() / it }
 infix fun List<Double>.Zdiv(other: Number) = this.map { it / other.toDouble() }
+infix fun Number.Zdiv(other: List2<Double>) = List2(other.map { this.toDouble() / it })
+infix fun List2<Double>.Zdiv(other: Number) = List2(this.map { it / other.toDouble() })
 
-// normalize list so that sum is 1
+        // normalize list so that sum is 1
 fun List<Double>.normalize(): List<Double> {
     val s = this.sum()
     if (s == 0.0) return this
     return this Zdiv s
 }
+fun List2<Double>.normalize(): List2<Double> {
+    val s = this.sum()
+    if (s == 0.0) return this
+    return this Zdiv s
+}
+
+fun List2<Double>.length() = sqrt(e1*e1 + e2*e2)
 
 fun <K,V,R> Map<K,V>.mapValuesNotNull(f: (Map.Entry<K,V>) -> R?) = mapNotNull {
     val mapped = f(it)
