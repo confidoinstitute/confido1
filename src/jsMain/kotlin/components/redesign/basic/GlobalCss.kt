f7b693097023945d0c7e8e0c6237bd9d2eb1934d
Jiří Sejkora <jirka@confido.tools>
2023-05-15 04:06:31 +0200

    Add a serif font-face

--- src/jsMain/kotlin/components/redesign/basic/GlobalCss.kt
+++ src/jsMain/kotlin/components/redesign/basic/GlobalCss.kt
@@ -11,7 +11,7 @@ external interface GlobalCssProps : Props {
 val sansSerif = string("Inter, sans-serif")
 val serif = string("TeX Gyre Pagella, Palatino, serif")
 
-val GlobalCss = FC<GlobalCssProps> {props ->
+val GlobalCss = FC<GlobalCssProps> { props ->
     Global {
         styles {
             "*" {
@@ -25,9 +25,58 @@ val GlobalCss = FC<GlobalCssProps> {props ->
                 display = Display.flex
                 flexDirection = FlexDirection.column
             }
+        }
+    }
+
+    // We need to use a separate Global per @font-face, as this is internally a javascript object
+    // and the repeated name would conflict. The wrapper API does not support passing in an array
+    // (see https://github.com/emotion-js/emotion/issues/1395), so we use multiple Globals instead.
+
+    Global {
+        styles {
             fontFace {
                 fontFamily = "Inter"
                 src = "url('/static/Inter.var.woff2') format('woff2')"
+            }
+        }
+    }
+    Global {
+        styles {
+            fontFace {
+                fontFamily = "TeX Gyre Pagella"
+                fontWeight = FontWeight.normal
+                fontStyle = FontStyle.normal
+                src = "url('/static/texgyrepagella-regular.woff2') format('woff2')"
+            }
+        }
+    }
+    Global {
+        styles {
+            fontFace {
+                fontFamily = "TeX Gyre Pagella"
+                fontWeight = FontWeight.normal
+                fontStyle = FontStyle.italic
+                src = "url('/static/texgyrepagella-italic.woff2') format('woff2')"
+            }
+        }
+    }
+    Global {
+        styles {
+            fontFace {
+                fontFamily = "TeX Gyre Pagella"
+                fontWeight = FontWeight.bold
+                fontStyle = FontStyle.normal
+                src = "url('/static/texgyrepagella-bold.woff2') format('woff2')"
+            }
+        }
+    }
+    Global {
+        styles {
+            fontFace {
+                fontFamily = "TeX Gyre Pagella"
+                fontWeight = FontWeight.bold
+                fontStyle = FontStyle.italic
+                src = "url('/static/texgyrepagella-bolditalic.woff2') format('woff2')"
             }
         }
     }
