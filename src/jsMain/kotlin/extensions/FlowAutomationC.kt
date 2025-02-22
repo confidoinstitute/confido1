3633b1541792c1d986c1ba7193d5991e772b553b
Filip Stedronsky <filip@confido.tools>
2025-02-22 17:33:01 +0100

    [ext/flow_automation] Support foreach loops over questions

--- src/jsMain/kotlin/extensions/FlowAutomationC.kt
+++ src/jsMain/kotlin/extensions/FlowAutomationC.kt
@@ -9,12 +9,16 @@ import components.rooms.RoomContext
 import csstype.TextDecoration
 import csstype.em
 import emotion.react.css
+import extensions.QuestionGroupsKey
 import hooks.useCoroutineLock
 import io.ktor.client.request.*
 import io.ktor.http.*
+import kotlinx.js.Object
+import kotlinx.js.jso
 import mui.material.FormGroup
 import react.*
 import react.dom.html.ReactHTML.a
+import react.dom.html.ReactHTML.body
 import react.dom.html.ReactHTML.li
 import react.dom.html.ReactHTML.ol
 import react.router.Route
@@ -22,8 +26,51 @@ import rooms.Room
 import rooms.RoomPermission
 import tools.confido.extensions.ClientExtension
 import tools.confido.extensions.get
+import tools.confido.question.Question
 import tools.confido.state.SentState
 import tools.confido.state.clientState
+import tools.confido.state.globalState
+
+external interface FlowItem {
+    var name: String?
+    var path: String?
+    var body: String?
+    var json: dynamic?
+    var forEach: String?
+    var block: Array<FlowItem>?
+    var multi: Array<FlowItem>?
+}
+
+fun getCollection(spec: String): List<Question> =
+    if (spec.startsWith("@")) {
+        val grp = spec.substring(1)
+        globalState.questions.values.filter { grp in it.extensionData[QuestionGroupsKey] }.toList()
+    } else {
+        spec.split(",").mapNotNull { globalState.questions[it] }
+    }
+
+fun expandFlowItem(item: FlowItem): Array<FlowItem> {
+    val forEach = item.forEach
+    val block = item.block
+    if (forEach != null) {
+        val bodyTemplate = jso<FlowItem>()
+        Object.assign(bodyTemplate, item)
+        bodyTemplate.forEach = null
+        val r = mutableListOf<FlowItem>()
+        getCollection(forEach).forEachIndexed { idx,q->
+            val body = JSON.parse<FlowItem>(JSON.stringify(bodyTemplate).replace("\$qid", q.id).replace("\$qname", q.name).replace("\$idx", (idx+1).toString()))
+            r.addAll(expandFlowItem(body))
+        }
+        return r.toTypedArray()
+    } else if (block != null) {
+        return block
+    } else {
+        return arrayOf(item)
+    }
+}
+
+fun expandFlow(flow: Array<FlowItem>) =
+    flow.flatMap { expandFlowItem(it).toList() }.toTypedArray()
 
 val FlowPage = FC<Props> {
     val room = useContext(RoomContext)
@@ -70,14 +117,15 @@ val FlowPage = FC<Props> {
         +"No flow defined for this room"
         return@FC
     }
-    var dynFlow  = arrayOf<dynamic>()
+    var dynFlow  = arrayOf<FlowItem>()
     try {
-        dynFlow = JSON.parse<Array<dynamic>>(flow)
+        dynFlow = JSON.parse(flow)
     } catch (e:Throwable
     ){
         console.log(e)
         +"Flow error: $e"
     }
+    dynFlow = expandFlow(dynFlow)
     console.log(dynFlow)
     ol {
         dynFlow.forEach { item->
